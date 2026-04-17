efe1b139f13cc77d7af0ccf1a1d569cd3c5ed96b
Dinithi-Dilshani <dilshanidinithi19@gmail.com>
2026-04-17 23:37:04 +0530

    update GlobalExceptionHandler.java and SecurityConfig.java files

--- backend/app/src/main/java/com/studentcampus/app/exception/GlobalExceptionHandler.java
+++ backend/app/src/main/java/com/studentcampus/app/exception/GlobalExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import com.studentcampus.app.exception.BookingConflictException; // ✅ ADDED
+
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
